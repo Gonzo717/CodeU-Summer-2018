faab1cf34df2af3c66100e81cc5fcc77dd332b67
Crista <cmondragon15117@gmail.com>
2018-05-27 03:21:24 +0000

    ActivityFeed data model + tests
    ActivityFeed data model added along with unit tests checking the creation of the three types of activity

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ActivityFeedServlet extends HttpServlet {
-	//implement doGet so it knows where to go
+
 	@Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
---
+++ src/main/java/codeu/model/data/Activity.java
@@ -0,0 +1,47 @@
+package codeu.model.data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/* Enum that specifies Activity Type
+ * newUser - user has joined
+ * newConvo - conversation has been made
+ * newMessage - message has been sent
+ */
+enum ActivityType{ newUser, newConvo, newMessage;}
+
+public class Activity {
+	public final ActivityType type;
+	public final UUID id;
+	public final Instant creation;
+	
+	/*
+	*	Constructs a new activity
+	*	
+	* @param type is the type of activity made
+	*	@param id is the ID of who ever made the activity
+	*	@param creation is the creation time of this activity
+	*/
+	public Activity(ActivityType type, UUID id, Instant creation) {
+		this.type = type;
+		this.id = id;
+		this.creation = creation;
+	
+	}
+	
+	/* Returns the activity type */
+	public ActivityType getType() {
+		return type;
+	}
+	
+	/* Returns the ID of the user who has initiated the activity */
+	public UUID getId() {
+		return id;
+	}
+	
+	/* Returns the creation time of this activity */
+	public Instant getCreation() {
+		return creation;
+	}
+
+}
