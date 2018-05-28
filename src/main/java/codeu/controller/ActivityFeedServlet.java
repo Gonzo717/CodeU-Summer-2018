0b5ba57f32d4c29526091b3fd6ae71441b19c124
Crista <cmondragon15117@gmail.com>
2018-05-28 00:01:00 +0000

    ActivityFeed Servlet Updated for storing
    Added activityStore setup in servlet

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -1,9 +1,7 @@
 package codeu.controller;
 
-import codeu.model.data.Conversation;
-import codeu.model.data.User;
-import codeu.model.store.basic.ConversationStore;
-import codeu.model.store.basic.UserStore;
+import codeu.model.data.Activity;
+import codeu.model.store.basic.ActivityStore;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
@@ -14,10 +12,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ActivityFeedServlet extends HttpServlet {
-
+	
+	/* Stores class that gives access to Activities */
+	private ActivityStore activityStore;
+	
+	void setActivityStore(ActivityStore activityStore) {
+		this.activityStore = activityStore;
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		setActivityStore(ActivityStore.getInstance());
+	}
+	
 	@Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
+    List<Activity> activities = activityStore.getAllActivities();
+    request.setAttribute("activities", activities);
     request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
   }
 
